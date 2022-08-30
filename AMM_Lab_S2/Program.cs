56b784ec543f3c92afbce99bff7c922301e92a86
Dario Turin <dturin1997@gmail.com>
2022-08-30 13:27:49 -0500

    Primer Commit

---
+++ AMM_Lab_S2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMM_Lab_S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("v4---------------v3");
+            Console.WriteLine(" |               |");
+            Console.WriteLine(" |               |");
+            Console.WriteLine("v1---------------v2");
+
+            Rectangulo rectangulo = new Rectangulo();
+
+            rectangulo.v1 =new Vertice { x = 0, y = 2 };
+
+            rectangulo.v2 = new Vertice { x = 6, y = 2 };
+
+            rectangulo.v3 = new Vertice { x = 6, y = 6 };
+
+            rectangulo.v4 = new Vertice { x = 0, y = 6 };
+
+            Console.WriteLine("El Area del Rectangulo es : ");
+            Console.WriteLine(rectangulo.CalcularArea());
+
+            Console.WriteLine("El Perímetro del Rectangulo es : ");
+            Console.WriteLine(rectangulo.CalcularPerimetro());
+
+            Console.Read();
+        }
+    }
+}
---
+++ AMM_Lab_S2/Rectangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMM_Lab_S2
+{
+    public class Rectangulo
+    {
+        public Vertice v1 { get; set; }
+        public Vertice v2 { get; set; }
+        public Vertice v3 { get; set; }
+        public Vertice v4 { get; set; }
+
+        public double CalcularArea()
+        {
+            double lado = Math.Sqrt(Math.Pow(v1.x - v2.x,2) + Math.Pow(v1.y - v2.y,2));
+            double altura = Math.Sqrt(Math.Pow(v2.x - v3.x, 2) + Math.Pow(v2.y - v3.y, 2));
+            double area=lado*altura;
+            return area;
+        }
+
+        public double CalcularPerimetro()
+        {
+            double lado = Math.Sqrt(Math.Pow(v1.x - v2.x, 2) + Math.Pow(v1.y - v2.y, 2));
+            double altura = Math.Sqrt(Math.Pow(v2.x - v3.x, 2) + Math.Pow(v2.y - v3.y, 2));
+            double perimetro = 2 * lado + 2 * altura; 
+            return perimetro;
+        }
+    }
+}
